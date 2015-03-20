0cefb033054c7aa2b39bb059431c42717f4c129a
Zoila <zoila@outlook.com>
2015-03-19 23:37:52 -0700

    Removed jQuery from Angular. Added two way data binding. Added Fisher-Yates algorithm for shuffle. - css - js - html

--- anagram.js
+++ anagram.js
@@ -5,93 +5,91 @@
  */
 var app = angular.module("Anagram", []);
 
-app.controller("AnagramController", function($scope, $filter, $timeout) {
-	  $scope.score = 0;
-	  $scope.answer = '';
-	  $scope.letter = '';
-	  $scope.circleTimer = $(".countdown").TimeCircles(); 
-	  $scope.inputAnswer = $("input[name=answer]");
-	  $scope.inputSend = $("input[name=send]");
-	  $scope.inputNext = $("input[name=next]");
+app.controller("AnagramController", ["$scope", "$timeout", function($scope, $timeout) {
+	  $scope.score;
+	  $scope.answer;
+	  $scope.letters = [];
+	  $scope.circleTimer;
+	  var letterAnimation = 0;
+	  var timerPromise;
 	  
-	  $scope.inputAnswer.focus();
-	  $scope.inputAnswer.keypress(function(e) {
-	  	if (e.keyCode === 13) {
-	  		$scope.inputSend.click();
-	  	}
-	  });
-	  
-	  $scope.correctWord = "LOVE";
-	  var words = ["VIOLET", "EGG", "BUTTER", "BERRY", "CRISPY", 
+	  var words = ["VIOLET", "EGG", "BUTTER", "BERRY", "CRISPY", "YELLOW", "GREEN",
 					  "BONUS", "MEXICO", "OPEN", "PAID", "GOOD", "TARGET", "MONEY", 
-					  "PEACE", "LOVE", "TIME", "FRIEND", "LIVE", "FLOWER", "CAT", "GREED",
-					  "BOAT", "HUMAN", "VOGUE", "WIRE", "GIRL", "BOY"];
-
-	
+					  "PEACE", "LOVE", "TIME", "FRIEND", "CHASE", "FLOWER", "GLASS", "GREED",
+					  "BOAT", "HUMAN", "VOGUE", "WIRE", "GIRL", "PHONE"];
+					  
+	  $scope.isLetterVisible = function() {
+	  	return letterAnimation === 1;
+	  }
+	  
+	  $scope.isLetterHiding = function() {
+	  	return letterAnimation === 2;
+	  }
+	  
+	  $scope.isCorrectLetter = function() {
+	  	return letterAnimation === 3;
+	  }
+	  
 	  $scope.validateWord = function() {
 		if($scope.circleTimer.getTime() > 0) {
-			var answer = $filter('uppercase')($scope.answer);
-			if (answer === $scope.correctWord) {
+			if ($scope.answer.toUpperCase() === $scope.correctWord) {
 		 		$scope.score += 1;
-		 		$scope.nextWord();
+		 		letterAnimation = 3;
+		 		timerPromise = $timeout($scope.displayWord, 1000);
 			 } 
 		} else {
 			alert("Timeout!");
 		}
-		
-		
 	}
 	
+
 	$scope.nextWord = function() {
-		$scope.greenFlag();
-		$timeout($scope.displayWord, 500);
-		
+		$scope.answer = ' ';
+		letterAnimation = 2;
+		timerPromise = $timeout($scope.displayWord, 1000);
 	}
 	
-	$scope.greenFlag = function() {
-		$(".letter").css("background-color","#449D44");
-    	$(".letter").slideToggle("1500");
-	}
 	
 	$scope.displayWord = function() {
-		$timeout.cancel($scope.displayWord);
-		$scope.inputAnswer.val('');
-		$scope.inputAnswer.focus();
+		$scope.answer = '';
 		var wordIndex = Math.floor(Math.random() * words.length);
 		$scope.correctWord = words[wordIndex];
-		var anagram = '';
-		var canvas = $("#canvas").empty();
-		var randomIndexes = $scope.getRandomIndexes();
-		for (var i = 0; i < $scope.correctWord.length; i++) {
-			$scope.letter = document.createElement('div');
-			anagram = $scope.correctWord.charAt(randomIndexes[i]);
-			$scope.letter.innerHTML = anagram;
-			$scope.letter.className = "letter fadeIn";
-			canvas.append($scope.letter);
-		}
+		$scope.letters = $scope.correctWord.split('');
+		$scope.getRandomIndexes();
+		timerPromise = $timeout(function() { letterAnimation = 1; }, 100);
 	}
 	
-	$scope.getRandomIndexes = function() {
-		var result = [];
-		var nonExist = true;
-		while (result.length < $scope.correctWord.length) {
-			nonExist = true;
-			var index = Math.floor(Math.random() * $scope.correctWord.length);
-			for (var i = 0; i < result.length; i++) {
-				if (result[i] === index) {
-					nonExist = false;
-					break;
-				}
-			}
-			if (nonExist) {
-				result.push(index);
-			} 
+		$scope.getRandomIndexes = function() {
+		var randomIndex;
+		var size = $scope.letters.length;
+		var temp;
+		while (size > 0) {
+			randomIndex = Math.floor(Math.random() * size--);
+			temp = $scope.letters[size];
+			$scope.letters[size] = $scope.letters[randomIndex];
+			$scope.letters[randomIndex] = temp;
 		}
-		
-		return result;
 	}
+	 
+	$scope.init = function() {
+	  	$scope.circleTimer = $(".countdown").TimeCircles(); 
+	  	$scope.score = 0;
+	  	$scope.answer = '';
+	  	$scope.displayWord();
+	}();
 
+}]);
+
+app.directive('focus', function() {
+  return {
+    restrict: 'A',
+    scope: { focus: '=' },
+    link: function(scope, elem) {
+      scope.$watch('focus', function(focus) {
+        if (focus)
+          elem[0].focus();
+      });
+    }
+  };
 });
-
-
 	
